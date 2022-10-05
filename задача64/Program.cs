09db5c062067f2723c8678a58de5287dd6356726
29Tatiana <amiravob@mail.ru>
2022-10-05 10:18:39 +0300

    задача64р

--- задача64/Program.cs
+++ задача64/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine (GetNaturalNumbers(N));
+
+string GetNaturalNumbers (int N)
+{
+      if (N == 1)
+            return "1";
+      return N + ", " + GetNaturalNumbers(N - 1);
+}
