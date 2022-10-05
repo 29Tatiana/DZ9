73f7f9758fd597984704557a2d0ef22e9254e772
29Tatiana <amiravob@mail.ru>
2022-10-05 11:17:38 +0300

    задача66р

--- задача66/Program.cs
+++ задача66/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputNumber("Ввод m: ");
+int n = InputNumber("Ввод n: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+PrintSum(m, n, temp = 0);
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма = {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+
+}
